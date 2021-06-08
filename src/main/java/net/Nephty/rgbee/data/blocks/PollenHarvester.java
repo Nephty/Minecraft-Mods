bfc09b57f88a5f4ea9b531f343402b7fb29e3962
Nephty <72945143+Nephty@users.noreply.github.com>
2021-06-08 22:51:36 +0200

    Updated src 2 : Introducing pollen harvester
    Player now has to use a pollen harvester to get pollen from the enchanted flower.
    Pollen harvester can be crafter using 4 slabs and 2 iron ingots.

---
+++ src/main/java/net/Nephty/rgbee/data/blocks/PollenHarvester.java
@@ -0,0 +1,31 @@
+package net.Nephty.rgbee.data.blocks;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.*;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class PollenHarvester extends ShearsItem {
+    // TODO : use pollenstack to stack the pollen in the pollen harvester and allow player to retrieve it through crafting
+    int pollenStack = 0;
+    public PollenHarvester(Properties p_i48471_1_) {
+        super(p_i48471_1_);
+    }
+
+    public PollenHarvester(Properties p_i48471_1_, int pollenStack) {
+        super(p_i48471_1_);
+        this.pollenStack = pollenStack;
+    }
+
+    @Override
+    public boolean mineBlock(ItemStack itemStack, World world, BlockState blockState, BlockPos blockPos, LivingEntity livingEntity) {
+        return true;
+    }
+
+    @Override
+    public ActionResultType interactLivingEntity(ItemStack stack, net.minecraft.entity.player.PlayerEntity playerIn, LivingEntity entity, net.minecraft.util.Hand hand) {
+        return ActionResultType.PASS;
+    }
+}
---
+++ src/main/java/net/Nephty/rgbee/setup/ModItems.java
@@ -0,0 +1,20 @@
+package net.Nephty.rgbee.setup;
+
+import net.Nephty.rgbee.data.blocks.PollenHarvester;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.fml.RegistryObject;
+
+public class ModItems {
+    public static final RegistryObject<Item> POLLEN = Registration.ITEMS.register("pollen", () ->
+            new Item(new Item.Properties().tab(ItemGroup.TAB_MATERIALS)));
+    public static final RegistryObject<Item> LIQUID_POLLEN = Registration.ITEMS.register("liquid_pollen", () ->
+            new Item(new Item.Properties().tab(ItemGroup.TAB_MATERIALS)));
+    public static final RegistryObject<Item> LIQUID_POLLEN_BOTTLE = Registration.ITEMS.register("liquid_pollen_bottle", () ->
+            new Item(new Item.Properties().tab(ItemGroup.TAB_MATERIALS)));
+    public static final RegistryObject<Item> POLLEN_HARVESTER = Registration.ITEMS.register("pollen_harvester", () ->
+            new PollenHarvester(new Item.Properties().tab(ItemGroup.TAB_TOOLS)));
+
+
+    static void register() {}
+}
